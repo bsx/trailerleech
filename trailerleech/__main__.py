3309bd88783c4dd022201d979b1a1190d85bd921
bsx <bsx+gh@0xcafec0.de>
2010-11-11 20:13:08 +0100

    replaced own horrible parsing with pytrailer

--- trailerleech/__main__.py
+++ trailerleech/__main__.py
@@ -3,19 +3,19 @@
 import sys
 
 import urllib2
-import json 
-import html5lib
+import pytrailer
+import re
 
-baseurl = "http://trailers.apple.com"
-trailerinclude = "includes/playlists/web.inc"
-movies = json.load(urllib2.urlopen(urllib2.Request(url="http://trailers.apple.com/trailers/home/feeds/genres.json",headers={"User-Agent": "Safari"})))
+resMatcher = re.compile(r"_h([0-9]+p?)\.mov")
 
-toload = {}
+jsonURL = "http://trailers.apple.com/trailers/home/feeds/genres.json"
+
+movies = pytrailer.getMoviesFromJSON(jsonURL)
 
 i = 0
 for movie in movies:
-    desc = ", ".join([x["type"] for x in movie["trailers"]])
-    print "%d\t%s [%s]" % (i, movie["title"], desc)
+    desc = ", ".join([x["type"] for x in movie.trailers])
+    print "%d\t%s [%s]" % (i, movie.title, desc)
     i += 1
 
 sys.stdout.write("Choose movie: ")
@@ -30,50 +30,9 @@ except:
 
 toload = movies[index]
 
-url = "%s%s%s" % (baseurl, toload["location"], trailerinclude)
-doc = html5lib.parse(urllib2.urlopen(urllib2.Request(url=url,headers={"User-Agent": "Safari"})), treebuilder="lxml")
-
-trailers = []
-
-match = doc.xpath("//h:ul[@class='trailer-nav']", namespaces={"h": "http://www.w3.org/1999/xhtml"})
-if len(match) > 0:
-    navigation = match[0]
-    for child in navigation.iterchildren(tag="{http://www.w3.org/1999/xhtml}li"):
-        name = child.xpath(".//h:span[@class='text']", namespaces={"h": "http://www.w3.org/1999/xhtml"})[0].text
-        trailers.append({"name": name})
-    content = doc.xpath("//h:div[@class='trailer-content']", namespaces={"h": "http://www.w3.org/1999/xhtml"})[0]
-    i = 0
-    for child in content.iterchildren(tag="{http://www.w3.org/1999/xhtml}div"):
-        trailer = trailers[i]
-        trailer["links"] = []
-        for link in child.iterdescendants(tag='{http://www.w3.org/1999/xhtml}a'):
-            img = link.getchildren()[0]
-            href = link.attrib["href"]
-            try:
-                res = img.attrib["alt"]
-                trailer["links"].append({"res": res, "url": href})
-            except:
-                pass
-        i += 1
-else:
-    match = doc.xpath("//h:li[contains(@class,'trailer')]", namespaces={"h": "http://www.w3.org/1999/xhtml"})
-    if len(match) > 0:
-        for section in match:
-            name = section.xpath(".//h:h3", namespaces={"h": "http://www.w3.org/1999/xhtml"})[0].text
-            trailer = {"name": name}
-            trailer["links"] = []
-            for link in section.xpath(".//h:a[@class='target-quicktimeplayer']", namespaces={"h": "http://www.w3.org/1999/xhtml"}):
-                href = link.attrib["href"]
-                res = link.text.split(" ")[0]
-                trailer["links"].append({"res": res, "url": href})
-            trailers.append(trailer)
-    else:
-        print "BUG: Couldn't parse %s" % url
-
 i = 0
-for trailer in trailers:
-    resolutions = ", ".join([x["res"] for x in trailer["links"]])
-    print "%d\t%s (%s)" % (i, trailer["name"], resolutions)
+for name, trailer in toload.trailerLinks.items():
+    print "%d\t%s" % (i, name)
     i += 1
 
 sys.stdout.write("Choose trailer: ")
@@ -86,11 +45,12 @@ except:
     print "You are a moron!"
     sys.exit(1)
 
-selected = trailers[index]
+selected = toload.trailerLinks.values()[index]
+trailerName = toload.trailerLinks.keys()[index]
 
 i = 0
-for link in selected["links"]:
-    print "%d\t%s" % (i, link["res"])
+for link in selected:
+    print "%d\t%s" % (i, link)
     i += 1
 
 sys.stdout.write("Choose resolution: ")
@@ -103,9 +63,14 @@ except:
     print "You are a moron!"
     sys.exit(1)
 
-link = selected["links"][index]
+link = selected[index]
+res = resMatcher.search(link)
+if res:
+    res = res.group(1)
+else:
+    res = "unknown"
 
-mov = urllib2.urlopen(urllib2.Request(url=link["url"],headers={"User-Agent": "QuickTime"}))
+mov = urllib2.urlopen(urllib2.Request(url=link,headers={"User-Agent": "QuickTime"}))
 size = int(mov.info().get("Content-Length"))
 if size < 200:
     content = mov.read()
@@ -115,7 +80,7 @@ if size < 200:
     mov = urllib2.urlopen(urllib2.Request(url=reallink,headers={"User-Agent": "QuickTime"}))
     size = int(mov.info().get("Content-Length"))
 
-filename = "%s - %s - %s.mov" % (toload["title"], selected["name"], link["res"])
+filename = "%s - %s - %s.mov" % (toload.title, trailerName, res)
 target = open(filename, "wb")
 print "Downloading %d bytes to %s" % (size, filename)
 
